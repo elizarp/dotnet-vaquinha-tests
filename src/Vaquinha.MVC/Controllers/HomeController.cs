f94dfb8b6b837102b49cd90746af45dbe4b0005c
freiremarcos <marcos.freire@zarpsystem.com.br>
2020-05-10 00:06:54 -0300

    home index

--- src/Vaquinha.MVC/Controllers/HomeController.cs
+++ src/Vaquinha.MVC/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Vaquinha.Domain;
+using Vaquinha.Domain.ViewModels;
 using Vaquinha.MVC.Models;
 
 namespace Vaquinha.MVC.Controllers
@@ -22,18 +24,19 @@ namespace Vaquinha.MVC.Controllers
         public async Task<IActionResult> Index()
         {
             var homeViewModel = await _homeService.RecuperarDadosIniciaisHomeAsync();
+
+            // mocado para testes de carousel ( remover aapós ajustes )
+            homeViewModel.Doadores = new List<DoadorViewModel> { new DoadorViewModel {Nome = "Doador 1"}, new DoadorViewModel { Nome = "Doador 2" } , new DoadorViewModel { Nome = "Doador 3" } };
+            homeViewModel.Instituicoes = new List<InstituicaoViewModel> { new InstituicaoViewModel { Nome = "Instituicao 1" }, new InstituicaoViewModel { Nome = "Instituicao 1" }, new InstituicaoViewModel { Nome = "Instituicao 3" } };
+
+            // adicionar carousel para instituicoes
+
             return View(homeViewModel);
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
