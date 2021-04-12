4e19cd318988c1ecdad22026dfe5f2df1984ec00
Philipp Moreira <philipp.moreira@hotmail.com>
2021-04-11 22:23:21 -0300

    Implementado testes de unidade da entidade Causa, quanto ao dominio da aplicacão/solucão.

---
+++ tests/Vaquinha.Tests.Common/Fixtures/CausaFixture.cs
@@ -0,0 +1,69 @@
+﻿using Bogus;
+using System;
+using Vaquinha.Domain.Entities;
+using Xunit;
+
+namespace Vaquinha.Tests.Common.Fixtures
+{
+	[CollectionDefinition(nameof(CausaFixtureCollection))]
+	public class CausaFixtureCollection : ICollectionFixture<CausaFixture>
+	{ }
+
+	public class CausaFixture
+	{
+		#region Atributos
+		private readonly string _localidade = "pt_BR";
+		#endregion
+
+		#region Métodos
+		public Causa CausaValida()
+		{
+			var faker = new Faker<Causa>(_localidade);
+
+			faker
+				.RuleFor(c => c.Id, f => Guid.NewGuid())
+				.RuleFor(c => c.Nome, f => f.Company.CompanyName())
+				.RuleFor(c => c.Cidade, f => f.Address.City())
+				.RuleFor(c => c.Estado, f => f.Address.StateAbbr());
+
+			return faker.Generate();
+		}
+
+		public Causa CausaVaziaInvalida()
+		{
+			var faker = new Faker<Causa>(_localidade);
+
+			faker
+				.RuleFor(c => c.Id, f => Guid.Empty)
+				.RuleFor(c => c.Nome, f => string.Empty)
+				.RuleFor(c => c.Cidade, f => string.Empty)
+				.RuleFor(c => c.Estado, f => string.Empty);
+
+			return faker.Generate();
+		}
+
+		public Causa CausaCamposNulosInvalida()
+		{
+			var faker = new Faker<Causa>(_localidade);
+
+			var causa = faker.CustomInstantiator(f => new Causa());
+
+			return causa;
+		}
+
+		public Causa CausaMaxLengthCamposExcedidoInvalida()
+		{
+			var quantidadeCaracteres = (CausaValidacao.MaxLengthCampos + 1);
+			var faker = new Faker<Causa>(_localidade);
+
+			faker
+				.RuleFor(c => c.Id, f => Guid.NewGuid())
+				.RuleFor(c => c.Nome, f => f.Lorem.Letter(quantidadeCaracteres))
+				.RuleFor(c => c.Cidade, f => f.Lorem.Letter(quantidadeCaracteres))
+				.RuleFor(c => c.Estado, f => f.Lorem.Letter(quantidadeCaracteres));
+
+			return faker.Generate();
+		}
+		#endregion
+	}
+}
---
+++ tests/Vaquinha.Unit.Tests/DomainTests/CausaTests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Vaquinha.Tests.Common.Fixtures;
+using Xunit;
+
+namespace Vaquinha.Unit.Tests.DomainTests
+{
+	[Collection(nameof(CausaFixtureCollection))]
+	public class CausaTests : IClassFixture<CausaFixture>
+	{
+
+		#region Atributos
+		private readonly CausaFixture _fixture;
+		#endregion
+
+		#region Construtores
+		public CausaTests(CausaFixture fixture)
+		{
+			_fixture = fixture;
+		}
+		#endregion
+
+		#region Métodos/Tests
+		[Fact]
+		[Trait("Causa", "CausaPreenchidaCorretamente_Valida")]
+		public void CausaPreenchidaCorretamente_Valida()
+		{
+			// Arrange
+			var causa = _fixture.CausaValida();
+
+			// Act
+			var valido = causa.Valido();
+
+			// Assert
+			valido.Should().BeTrue(because: "Todos os campos estão preenchidos conforme o estabelecido.");
+
+			causa.ErrorMessages.Should().BeNullOrEmpty(because: "Pois todos os campos estão preenchidos conforme o estabelecido, não devendo criar ou constar nenhum mensagem de validacão violada.");
+		}
+
+		[Fact]
+		[Trait("Causa", "CausaPreenchidaCamposNulos_Invalida")]
+		public void CausaPreenchidaCamposNulos_Invalida()
+		{
+			// Arrange
+			var causa = _fixture.CausaCamposNulosInvalida();
+
+			// Act
+			var valido = causa.Valido();
+
+			// Assert
+			valido.Should().BeFalse(because: "todos os campos/propriedades estão com valor padrão/default de seu(s) respectivo(s) tipo.");
+
+			causa.ErrorMessages.Should().HaveCountGreaterOrEqualTo(expected: 1, because: "ao menos um do(s) campo(s) contemplado(s) na validacão da entidade/causa, deveria ter constado.");
+		}
+
+		[Fact]
+		[Trait("Causa", "CausaCamposEmBranco_Invalida")]
+		public void CausaCamposEmBranco_Invalida()
+		{
+			// Arrange
+			var causa = _fixture.CausaVaziaInvalida();
+
+			// Act
+			var valido = causa.Valido();
+
+			// Assert
+			valido.Should().BeFalse(because: "os campos foram informados com o valor de vazio/empty.");
+
+			causa.ErrorMessages.Should().HaveCountGreaterOrEqualTo(expected: 1, because: "ao menos um do(s) campo(s) contemplado(s) na validacão da entidade/causa, deveria ter constado.");
+		}
+
+		[Fact]
+		[Trait("Causa", "CamposComQuantidadeCaracteresExcecida_Invalida")]
+		public void CamposComQuantidadeCaracteresExcecida_Invalida()
+		{
+			// Arrange
+			var causa = _fixture.CausaMaxLengthCamposExcedidoInvalida();
+
+			// Act
+			var valido = causa.Valido();
+
+			// Assert
+			valido.Should().BeFalse(because: "todos os campos da entidade/causa foram preenchidos excedendo o tamanho máximo esperado.");
+
+			causa.ErrorMessages.Should().HaveCountGreaterOrEqualTo(expected: 1, because: "ao menos um do(s) campo(s) contemplado(s) na validacão da entidade/causa, deveria ter constado.");
+		}
+
+		#endregion
+	}
+}
