18edcd2bf57f62ded6779862c508ac484f085a79
Philipp Moreira <philipp.moreira@hotmail.com>
2021-04-17 08:42:25 -0300

    Removido o método PossuiErrosDominio(), pois, agora está implementado na parent class EntidadeBase.cs.

--- src/Vaquinha.MVC/Controllers/DoacoesController.cs
+++ src/Vaquinha.MVC/Controllers/DoacoesController.cs
@@ -6,49 +6,45 @@ using Vaquinha.Domain.ViewModels;
 
 namespace Vaquinha.MVC.Controllers
 {
-    public class DoacoesController : BaseController
-    {
-        private readonly IDoacaoService _doacaoService;
-        private readonly IDomainNotificationService _domainNotificationService;
+	public class DoacoesController : BaseController
+	{
+		private readonly IDoacaoService _doacaoService;
+		private readonly IDomainNotificationService _domainNotificationService;
 
-        public DoacoesController(IDoacaoService doacaoService,
-                                 IDomainNotificationService domainNotificationService,
-                                 IToastNotification toastNotification) : base(domainNotificationService, toastNotification)
-        {
-            _doacaoService = doacaoService;
-            _domainNotificationService = domainNotificationService;
-        }
+		public DoacoesController(IDoacaoService doacaoService,
+								 IDomainNotificationService domainNotificationService,
+								 IToastNotification toastNotification) : base(domainNotificationService, toastNotification)
+		{
+			_doacaoService = doacaoService;
+			_domainNotificationService = domainNotificationService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            return View(nameof(Index), await _doacaoService.RecuperarDoadoresAsync());
-        }
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			return View(nameof(Index), await _doacaoService.RecuperarDoadoresAsync());
+		}
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Create(DoacaoViewModel model)
-        {
-            _doacaoService.RealizarDoacaoAsync(model);
+		[HttpPost]
+		public IActionResult Create(DoacaoViewModel model)
+		{
+			_doacaoService.RealizarDoacaoAsync(model);
 
-            if (PossuiErrosDominio())
-            {
-                AdicionarErrosDominio();
-                return View(model);
-            }
+			if (PossuiErrosDominio())
+			{
+				AdicionarErrosDominio();
+				return View(model);
+			}
 
-            AdicionarNotificacaoOperacaoRealizadaComSucesso("Doação realizada com sucesso!<p>Obrigado por apoiar nossa causa :)</p>");
-            return RedirectToAction("Index", "Home");
-        }
+			AdicionarNotificacaoOperacaoRealizadaComSucesso("Doação realizada com sucesso!<p>Obrigado por apoiar nossa causa :)</p>");
+			return RedirectToAction("Index", "Home");
 
-        private bool PossuiErrosDominio()
-        {
-            return _domainNotificationService.PossuiErros;
-        }
-    }
+		}
+	}
 }
