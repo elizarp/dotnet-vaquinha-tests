a8fdbb28f6084518c6d63bce8024dfd4e0125599
Philipp Moreira <philipp.moreira@hotmail.com>
2021-04-17 09:06:39 -0300

    Implementado a funcionalidade de Listagem e inclusão (somente) de causas.

---
+++ src/Vaquinha.MVC/Controllers/CausaController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NToastNotify;
+using System.Threading.Tasks;
+using Vaquinha.Domain;
+using Vaquinha.Domain.ViewModels;
+
+namespace Vaquinha.MVC.Controllers
+{
+	public class CausaController : BaseController
+	{
+		#region Atributos
+		private readonly ICausaService _service;
+		#endregion
+
+		#region Construtores
+		public CausaController(
+			ICausaService service,
+			IDomainNotificationService notifications,
+			IToastNotification toast)
+			: base(notifications, toast)
+		{
+			_service = service;
+		}
+		#endregion
+
+		#region Métodos / Acões
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			try
+			{
+				var causas = await _service.RecuperarCausas();
+
+				return View(model: causas);
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			try
+			{
+				return View();
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(CausaViewModel model)
+		{
+			try
+			{
+				await _service.AdicionarAsync(model);
+
+				if (PossuiErrosDominio())
+				{
+					AdicionarErrosDominio();
+					return View(model);
+				}
+
+				AdicionarNotificacaoOperacaoRealizadaComSucesso("Causa cadastrada com sucesso!");
+				return RedirectToAction("Index");
+			}
+			catch
+			{
+				throw;
+			}
+		}
+
+		#endregion
+	}
+}
--- src/Vaquinha.Service/CausaService.cs
+++ src/Vaquinha.Service/CausaService.cs
@@ -7,28 +7,38 @@ using Vaquinha.Domain.ViewModels;
 
 namespace Vaquinha.Service
 {
-    public class CausaService : ICausaService
-    {
-        private readonly IMapper _mapper;
-        private readonly ICausaRepository _causaRepository;
+	public class CausaService : ICausaService
+	{
+		private readonly IMapper _mapper;
+		private readonly ICausaRepository _causaRepository;
+		private readonly IDomainNotificationService _domainNotificationService;
 
-        public CausaService(ICausaRepository causaRepository,
-                            IMapper mapper)
-        {
-            _mapper = mapper;
-            _causaRepository = causaRepository;
-        }
+		public CausaService(IMapper mapper,
+							 ICausaRepository causaRepository,
+							 IDomainNotificationService domainNotificationService)
+		{
+			_mapper = mapper;
+			_causaRepository = causaRepository;
+			_domainNotificationService = domainNotificationService;
+		}
 
-        public async Task Adicionar(CausaViewModel model)
-        {
-            var causa = _mapper.Map<CausaViewModel, Causa>(model);
-            await _causaRepository.Adicionar(causa);
-        }
+		public async Task AdicionarAsync(CausaViewModel model)
+		{
+			var causa = _mapper.Map<CausaViewModel, Causa>(model);
 
-        public async Task<IEnumerable<CausaViewModel>> RecuperarCausas()
-        {
-            var causas = await _causaRepository.RecuperarCausas();
-            return _mapper.Map<IEnumerable<Causa>, IEnumerable<CausaViewModel>>(causas);
-        }
-    }
+			if (causa.Valido())
+			{
+				await _causaRepository.AdicionarAsync(causa);
+				return;
+			}
+
+			_domainNotificationService.Adicionar(causa);
+		}
+
+		public async Task<IEnumerable<CausaViewModel>> RecuperarCausas()
+		{
+			var causas = await _causaRepository.RecuperarCausas();
+			return _mapper.Map<IEnumerable<Causa>, IEnumerable<CausaViewModel>>(causas);
+		}
+	}
 }
